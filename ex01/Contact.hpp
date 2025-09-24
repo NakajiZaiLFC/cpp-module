4faaea11f4576f0e36deffe978b02dc0ca004911
Koki Fujita <kofujita@c5r4s8.42tokyo.jp>
2025-09-24 21:45:01 +0900

    Add: ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,47 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+#include <iostream>
+
+class Contact
+{
+	public:
+		Contact();
+		Contact(int m_index);
+		Contact(const Contact &other);
+		Contact &operator=(const Contact &other);
+		~Contact();
+
+		// ===== index =====
+		void setId(int id);
+		int getId() const;
+
+		// ===== first name =====
+		void setFirstName(std::string first_name);
+		std::string getFirstName() const;
+
+		// ===== last name =====
+		void setLastName(std::string last_name);
+		std::string getLastName() const;
+
+		// ===== nickname =====
+		void setNickname(std::string nickname);
+		std::string getNickname() const;
+
+		// ===== phone number =====
+		void setPhoneNum(std::string phone_num);
+		std::string getPhoneNum() const;
+
+		// ===== darkset secret =====
+		void setDarksetSecret(std::string darkset_secret);
+		std::string getDarksetSecret() const;
+
+	private:
+		int m_id;
+		std::string m_first_name;
+		std::string m_last_name;
+		std::string m_nickname;
+		std::string m_phone_num;
+		std::string m_darkset_secret;
+};
+
+#endif
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,52 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "Contact.hpp"
+#include <iomanip>
+#include <sstream>
+
+#define SAY_HI "Hi! This is My Awesome PhoneBook!\n"
+#define SELECT_PROMPT "Notice: You can use \"ADD\",\"SEARCH\",\"EXIT\"\n"
+#define SAY_GOODBYE "Thank you for using My Awesome PhoneBook!\n"
+#define PHONEBOOK_IS_EMPTY "Error: PhoneBook is Empty"
+#define COMMAND_IS_INVALID "Error: Command is Invalid\nNotice: You can use \"ADD\",\"SEARCH\",\"EXIT\"\n"
+#define INVALID_INDEX "Error: This index is Invalid\n"
+#define INVALID_INPUT "Error: The input str is Invalid\nPlease try agein!\n"
+#define INPUT_CANCLELED "Notice: Input Canceled\n"
+
+#define FIRST_NAME "First Name: "
+#define LAST_NAME "Last Name: "
+#define NICKNAME "Nickname: "
+#define PHONE_NUM "Phone Number: "
+#define DARKSET_SECRET "Darkset Secret: "
+#define ADD_DONE "New contact created!"
+
+class PhoneBook
+{
+public:
+	PhoneBook();
+	PhoneBook(const PhoneBook &other);
+	PhoneBook &operator=(const PhoneBook &other);
+	~PhoneBook();
+	void addContact(const Contact &new_contact);
+	void displayIndexRecord(int m_index);
+	bool displaySummaryList(void);
+	Contact m_contacts[8];
+	int m_index;
+	int m_contactCount;
+	bool index_validation(int search_index);
+	void handleSearchCommand();
+	void handleAddCommand();
+	bool strValidate(std::string str, std::string prompt);
+	bool phoneNumValidate(std::string str);
+	bool handlePrompt(std::string prompt, std::string &tmp_variable, Contact &tmp_contact);
+	void callSetter(std::string prompt, std::string &tmp_valiable, Contact &new_contact);
+	bool strIsSpace(std::string str);
+	std::string formatForDisplay(const std::string &str);
+
+private:
+	void displayColumn(int i);
+	void displayHeader(void);
+};
+
+#endif
