67834e4631a97c0c2206db0720d22a99aafff0ac
snakajim <snakajim@student.42tokyo.jp>
2025-09-25 18:44:36 +0900

    【修正】指摘部分を修正

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
+#define CASE_ARGC_1 1
 
 int main(int argc, char **argv)
 {
 	char c;
+
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
+		return (CASE_ARGC_1);
 	}
-	for (int i = 1; argv[i] != '\0'; i++)
+	for (int i = 1; i < argc; i++)
 	{
 		for (int j = 0; argv[i][j] != '\0'; j++)
-		{
-			c = std::toupper(argv[i][j]);
-			std::cout << c;
-		}
+			std::cout << std::toupper(argv[i][j]);
 	}
 	std::cout << std::endl;
 	return (0);
